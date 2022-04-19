d1eac70e71ac9bd3a913960421fed25a1c1a8584
Mahdi Bouaziz <mahdibouaziz@hotmail.fr>
2022-04-19 14:21:42 +0100

    associate the main route table and added access to the internet

--- main.tf
+++ main.tf
@@ -48,22 +48,36 @@ resource "aws_internet_gateway" "myapp-igw" {
   }
 }
 
-# Create a new route table with 2 rules (for local and internet )
-resource "aws_route_table" "myapp-route-table" {
-    vpc_id = aws_vpc.myapp-vpc.id
+# # Create a new route table with 2 rules (for local and internet )
+# resource "aws_route_table" "myapp-route-table" {
+#     vpc_id = aws_vpc.myapp-vpc.id
+#     route {
+#         cidr_block = "0.0.0.0/0"
+#         gateway_id = aws_internet_gateway.myapp-igw.id
+#     }
+
+#     tags = {
+#         Name = "${var.env_prefix}-rtb"
+#     }
+# }
+
+
+# # Associate the subnet to the route table created
+# resource "aws_route_table_association" "a" {
+#   subnet_id      = aws_subnet.myapp-subnet-1.id
+#   route_table_id = aws_route_table.myapp-route-table.id
+# }
+
+# Associate the default route table (main) and added it a new route
+resource "aws_default_route_table" "main-rtb" {
+    default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
+
     route {
         cidr_block = "0.0.0.0/0"
         gateway_id = aws_internet_gateway.myapp-igw.id
     }
 
     tags = {
-        Name = "${var.env_prefix}-rtb"
+        Name = "${var.env_prefix}-main-rtb"
     }
-}
-
-
-# Associate the subnet to the route table created
-resource "aws_route_table_association" "a" {
-  subnet_id      = aws_subnet.myapp-subnet-1.id
-  route_table_id = aws_route_table.myapp-route-table.id
 }
