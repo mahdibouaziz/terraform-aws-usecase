fd39b4950a361c0e92c99e40ed78549c8919f8bb
Mahdi Bouaziz <mahdibouaziz@hotmail.fr>
2022-04-19 13:38:14 +0100

    added the vpc and subnet provisioning

---
+++ main.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "eu-west-3"
+}
+
+# create our variables
+variable "vpc_cidr_block" {}
+
+variable "subnet_cidr_block" {}
+
+variable "avail_zone" {}
+
+variable "env_prefix" {}
+
+# create a vpc
+resource "aws_vpc" "myapp-vpc" {
+  cidr_block = var.vpc_cidr_block
+  tags = {
+    Name = "${var.env_prefix}-vpc"
+  }
+}
+
+#create a subnet for this vpc
+resource "aws_subnet" "myapp-subnet-1" {
+  vpc_id     = aws_vpc.myapp-vpc.id
+  cidr_block = var.subnet_cidr_block
+  availability_zone = var.avail_zone
+  tags = {
+    Name = "${var.env_prefix}-subnet-1"
+  }
+}
