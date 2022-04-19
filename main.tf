147c510493476f44e25c2983239ca11e3102da4c
Mahdi Bouaziz <mahdibouaziz@hotmail.fr>
2022-04-19 14:09:34 +0100

    Created table route and internet gateway to connect to VPC to the internet and then associate the tabe route to the subnet

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ provider "aws" {
   region = "eu-west-3"
 }
 
-# create our variables
+# Create our variables
 variable "vpc_cidr_block" {}
 
 variable "subnet_cidr_block" {}
@@ -21,7 +21,7 @@ variable "avail_zone" {}
 
 variable "env_prefix" {}
 
-# create a vpc
+# Create a vpc
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cidr_block
   tags = {
@@ -29,7 +29,7 @@ resource "aws_vpc" "myapp-vpc" {
   }
 }
 
-#create a subnet for this vpc
+# Create a subnet for this vpc
 resource "aws_subnet" "myapp-subnet-1" {
   vpc_id     = aws_vpc.myapp-vpc.id
   cidr_block = var.subnet_cidr_block
@@ -37,4 +37,33 @@ resource "aws_subnet" "myapp-subnet-1" {
   tags = {
     Name = "${var.env_prefix}-subnet-1"
   }
+}
+
+# Create an internet gateway to assign it to the routetable 
+resource "aws_internet_gateway" "myapp-igw" {
+  vpc_id = aws_vpc.myapp-vpc.id
+
+  tags = {
+     Name = "${var.env_prefix}-igw"
+  }
+}
+
+# Create a new route table with 2 rules (for local and internet )
+resource "aws_route_table" "myapp-route-table" {
+    vpc_id = aws_vpc.myapp-vpc.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.myapp-igw.id
+    }
+
+    tags = {
+        Name = "${var.env_prefix}-rtb"
+    }
+}
+
+
+# Associate the subnet to the route table created
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.myapp-subnet-1.id
+  route_table_id = aws_route_table.myapp-route-table.id
 }
